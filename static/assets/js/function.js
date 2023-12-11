b7e8f54db05ba6e110e6192fc34233de28c57f9b
YomalHerath <yomalherath1121@gmail.com>
2023-12-11 06:11:27 +0000

    minor changes

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -1,58 +1,62 @@
+// Submitting a review form via AJAX
 console.log("Done");
 
-$("#new-review-form").submit(function(e){
+$("#new-review-form").submit(function (e) {
     e.preventDefault();
 
-    $ajax({
+    // AJAX request to save the review
+    $.ajax({
         data: $(this).serialize(),
-
         method: $(this).attr("method"),
-
         url: $(this).attr("action"),
-
         dataType: "json",
-
-        success: function(response){
+        success: function (response) {
             console.log("Saved to Database");
 
-            if(response.bool == True){
-                $("#review-success").html("Review Added Successfully.")
-                $("#hidden-comment-form").hide()
+            // If the review was successfully saved
+            if (response.bool == true) {
+                $("#review-success").html("Review Added Successfully.");
+                $("#hidden-comment-form").hide();
 
-                let _html = '<div class="spr-review">'
-                    _html += '<div class="spr-review-header">'
-                    _html += '<span class="product-review spr-starratings spr-review-header-starratings">'
-                    _html += '<span class="reviewLink">'
-                    
-                    for(let i=1; i<=response.context.rating; i++){
-                        _html += '<i class="fa fa-star text-warning "></i>'
-                    }
+                // Create HTML for displaying the new review
+                let _html = '<div class="spr-review">';
+                _html += '<div class="spr-review-header">';
+                _html += '<span class="product-review spr-starratings spr-review-header-starratings">';
+                _html += '<span class="reviewLink">';
 
-                    _html += '</span></span>'
-                    _html += '<h4 class="spr-review-header-title">'+ response.context.user +'</h4>'
-                    _html += '<span class="spr-review-header-byline"><strong>{{ r.date|date:"d M, Y" }}</strong></span>'
-                    _html += '</div>'
+                // Display stars based on the rating
+                for (let i = 1; i <= response.context.rating; i++) {
+                    _html += '<i class="fa fa-star text-warning "></i>';
+                }
 
-                    _html += '<div class="spr-review-content">'
-                    _html += '<p class="spr-review-content-body">'+ response.context.review +'</p>'
-                    _html += '</div>'
-                    _html += '</div>'
+                _html += '</span></span>';
+                _html += '<h4 class="spr-review-header-title">' + response.context.user + '</h4>';
+                _html += '<span class="spr-review-header-byline"><strong>{{ r.date|date:"d M, Y" }}</strong></span>';
+                _html += '</div>';
 
-                    $(".spr-reviews").prepend(_html)
+                _html += '<div class="spr-review-content">';
+                _html += '<p class="spr-review-content-body">' + response.context.review + '</p>';
+                _html += '</div>';
+                _html += '</div>';
+
+                // Prepend the new review to the reviews container
+                $(".spr-reviews").prepend(_html);
             }
         }
-    })
-})
+    });
+});
 
-
+// Filtering products based on checkbox selections
 $(document).ready(function () {
     console.log("Script loaded");
 
     $(".filter-checkbox").on("click", function () {
         console.log("Checkbox clicked");
 
+        // Object to store selected filter values
         let filter_objects = {};
 
+        // Iterate through each checkbox
         $(".filter-checkbox").each(function () {
             let filter_value = $(this).val();
             let filter_key = $(this).data("filter");
@@ -60,10 +64,12 @@ $(document).ready(function () {
             console.log("filter Value:", filter_value);
             console.log("filter Key:", filter_key);
 
+            // Initialize an array for each filter key if not present
             if (!(filter_key in filter_objects)) {
                 filter_objects[filter_key] = [];
             }
 
+            // Add the checked filter values to the array
             if ($(this).is(":checked")) {
                 filter_objects[filter_key].push(filter_value);
             }
@@ -71,6 +77,7 @@ $(document).ready(function () {
 
         console.log("Filter Objects: ", filter_objects);
 
+        // AJAX request to filter products
         $.ajax({
             url: '/filter-products',
             data: filter_objects,
@@ -83,6 +90,7 @@ $(document).ready(function () {
                 console.log(response);
                 console.log("Data Filtered Successfully");
 
+                // Update the HTML content with filtered products
                 $("#filtered-product").html(response.data);
             }
         });
